853355e6550e058dddfc2b39dcd59f0a8586c484
Robert Godlewski <robtechg5@gmail.com>
2022-06-05 12:32:31 -0700

    Added an admin user and updated the models to admin.

--- server/blog/models.py
+++ server/blog/models.py
@@ -6,12 +6,12 @@ from django.utils import timezone
 
 # Blog Models.
 class BlogPost(models.Model):
-    # id is automatically created?
+    # id is automatically created after making migrations
     title = models.CharField(max_length=100)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
-    #img_link = models.URLField() or models.ImageField() - Need to figure out details
-    #stream_link = models.URLField() - Need to figure out details
+    #img_link = models.ImageField(upload_to=None) - Need to edit
+    #stream_link = models.URLField(max_length=400) - Need to edit
     created_at = models.DateTimeField(timezone.now())
     updated_at = models.DateTimeField(timezone.now())
